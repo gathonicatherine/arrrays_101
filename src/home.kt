5b53b9bd471f36d32b3864a6b110be755e2119da
gathonicatherine <catherrinewanjiku@gmail.com>
2021-02-20 13:42:53 +0300

    array_101

---
+++ src/home.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var name="cate"
+    name="val"
+    name="neema"
+    name="uwiii"
+    var names=arrayOf("cate","val","neema","uwiii")
+    var numbers=arrayOf("cate",19,5.7,true)
+
+
+}
